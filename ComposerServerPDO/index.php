ba2341eaf83ca3680523f946bf8f802a151a8579
Gabriel <gmmanjos@gmail.com>
2020-09-17 14:15:34 -0300

    Curso

---
+++ ComposerServerPDO/index.php
@@ -0,0 +1,14 @@
+<?php
+// SETS
+require_once 'vendor/autoload.php';
+
+$produtoDao = new \App\Model\ProdutoDao();
+$produtoDao->delete(8);
+
+$produtoDao->read();
+
+foreach($produtoDao->read() as $produto):
+	echo $produto['nome']."<br>".$produto['descricao']."<hr>";
+endforeach;
+
+
