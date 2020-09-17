d7feb9f6094e7e71f3161ca6e10861f300ca5f7d
Gabriel <gmmanjos@gmail.com>
2020-09-17 10:33:45 -0300

    Curso

---
+++ Exceptions/index.php
@@ -0,0 +1,21 @@
+<?php
+
+ class network{
+     
+    public function cadastrarEmail($email){
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)):
+            throw new Exception("Email Invalido",123);
+        else:
+            echo "Email Válido <br>Cadastrado com sucesso";
+        endif;
+    }
+
+}
+$Network = new network();
+    try{
+        $Network->cadastrarEmail("Joao@.com.br"); 
+    } catch(Exception $e){
+        echo "Messagem:".$e->getMessage();
+        echo "<br>Código:".$e->getCode();
+    }
